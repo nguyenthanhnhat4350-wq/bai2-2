39edd49ecf2ec3337f8c429eb798d1528b3b4ffe
nguyenthanhnhat4350 <thiennguyen29055@gmail.com>
2026-02-02 10:54:00 +0700

    addmutiplefiles

--- src/Component/SubjectList.jsx
+++ src/Component/SubjectList.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import "./SubjectList.css"
+import "./SubjectList.css";
 
 function SubjectList(props) {
   return (
     <div className="subject-container">
-        <h2 className="subject-title">Danh sách môn học</h2>
-    <ul className="subject-list">
-      {props.subjects.map((item, index) => (
-        <li className="subject-item" key={index}>
-            {subject}
-        </li>
-      ))}
-    </ul>
+      <h2 className="subject-title">Danh sách môn học</h2>
+      <ul className="subject-list">
+        {props.subjects.map((item, index) => (
+          <li className="subject-item" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
